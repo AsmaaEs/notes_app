8f9cdff2f618980517495f433c9b17c20f5f23ab
Esmaa.osama <samaes755@gmail.com>
2023-08-25 15:02:58 +0300

    add hive

--- lib/views/widgets/custom_item_card.dart
+++ lib/views/widgets/custom_item_card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/views/edit_note_view.dart';
-import 'package:notes_app/views/widgets/edit_note_view_body.dart';
 
 class CustomItemCard extends StatelessWidget {
   const CustomItemCard({super.key});
